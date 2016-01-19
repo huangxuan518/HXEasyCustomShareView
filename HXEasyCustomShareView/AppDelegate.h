369f553a95368cb1d3ba1234b5713b7a80dfcbfc
黄轩 <huangxuan@fangdd.com>
2016-01-19 15:10:58 +0800

    Initial Commit

---
+++ HXEasyCustomShareView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXEasyCustomShareView
+//
+//  Created by 黄轩 on 16/1/19.
+//  Copyright © 2016年 IT小子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXEasyCustomShareView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXEasyCustomShareView
+//
+//  Created by 黄轩 on 16/1/19.
+//  Copyright © 2016年 IT小子. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
